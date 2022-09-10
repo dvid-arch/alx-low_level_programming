7c08802d13a839478e7b8bcbd18a660081b10d18
dvid-arch <derrickemma44@gmail.com>
2022-09-10 05:52:55 -0700

    new

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success/correct)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
 int main(void)
 
 {
@@ -14,26 +14,25 @@ int center;
 
 for (left = 48; left <= 57; left++)
 {
-for (center = left + 1; center <= 57; center++)
-{
-for (right = center + 1 ; right <= 57; right++)
+	for (center = left + 1; center <= 57; center++)
+	{
+		for (right = center + 1; right <= 57; right++)
+		{
+		putchar(left);
+		putchar(center);
+		putchar(right);
 
+		if ((left == 55) && (center == left + 1) && (right == center + 1))
+		{
+			break;
+		}
 
-{
-putchar(left);
-putchar(center);
-putchar(right);
+		putchar(',');
+		putchar(' ');
 
-if ((left == 55) && (center == left + 1) && (right == center + 1))
-{
-break;
-}
+		}
 
-putchar(',);
-putchar (' ');
-
-}
-}
+	}
 }
 
 putchar('\n');
