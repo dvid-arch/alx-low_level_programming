a94f787d41ebe6b41bdf23716931e7e016a2db66
dvid-arch <derrickemma44@gmail.com>
2022-09-10 05:36:34 -0700

    new

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 	int c, i;
-	
 	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
@@ -17,7 +16,6 @@ int main(void)
 			{
 				putchar(c);
 				putchar(i);
-
 				if (c != '8' || (c == '8' && i != '9'))
 				{
 					putchar(',');
