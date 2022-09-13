f7d522409f0eb167f26deab35442f4e5da8df4ce
dvid-arch <derrickemma44@gmail.com>
2022-09-13 07:46:26 -0700

    new

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - check the code
+* print_alphabet - prints lowercase alphabets
+* Return: 0
+*/
+int print_alphabet(void)
+{
+	int i;
+
+	for(int i = 'a'; i < 'z' i++)
+	{
+		putchar("%s", i)
+	}
+	
+	putchar("\n");
+
+	return 0;
+}
